32c3d124211328e7bb77b12301fbe85b50070088
root-lucas <945832032@qq.com>
2020-08-23 23:03:31 +0800

    feat: login routing permissions

--- src/main.js
+++ src/main.js
@@ -10,6 +10,17 @@ Vue.use(ElementUI, {
     size: 'small',
 })
 
+//使用钩子函数对路由进行权限跳转
+router.beforeEach((to, from, next) => {
+    document.title = `${to.meta.title} | vue-admin`
+    const role = localStorage.getItem('ms_username')
+    if (!role && to.path !== '/login') {
+        next('/login')
+    } else {
+        next()
+    }
+})
+
 new Vue({
     router,
     store,
--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,15 @@ const routes = [
         component: () => import('../components/page/Login.vue'),
         meta: { title: '登录' },
     },
+    {
+        path: '/404',
+        component: () => import('../components/page/404.vue'),
+        meta: { title: '404' },
+    },
+    {
+        path: '*',
+        redirect: '/404',
+    },
 ]
 
 const router = new VueRouter({
