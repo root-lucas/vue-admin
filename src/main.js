e04a5e137af1c2845743d06a6730b5dbb17d31f7
root-lucas <945832032@qq.com>
2020-08-23 22:34:31 +0800

    add login page

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
-Vue.use(ElementUI)
+Vue.use(ElementUI, {
+    size: 'small',
+})
 
 new Vue({
     router,
