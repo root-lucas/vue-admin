b6b632371aea5b8a4fb17f3d9086f76bca2e3423
root-lucas <945832032@qq.com>
2020-08-25 23:10:55 +0800

    add: systemHome page

--- src/components/page/Dashboard.vue
+++ src/components/page/Dashboard.vue
@@ -1,9 +1,334 @@
 <template>
-    <h1>dashboard</h1>
+    <div>
+        <el-row :gutter="20">
+            <el-col :span="8">
+                <el-card shadow="hover" class="mgb20" style="height:252px;">
+                    <div class="user-info">
+                        <img src="../../assets/img/avatar.jpg" class="user-avator" alt />
+                        <div class="user-info-cont">
+                            <div class="user-info-name">admin</div>
+                            <div>{{ role }}</div>
+                        </div>
+                    </div>
+                    <div class="user-info-list">
+                        上次登录时间：
+                        <span>2020-08-01</span>
+                    </div>
+                    <div class="user-info-list">
+                        上次登录地点：
+                        <span>广州</span>
+                    </div>
+                </el-card>
+                <el-card shadow="hover" style="height:252px;">
+                    <div slot="header" class="clearfix">
+                        <span>语言详情</span>
+                    </div>
+                    Vue
+                    <el-progress :percentage="71.3" color="#42b983"> </el-progress>
+                    JavaScript
+                    <el-progress :percentage="24.1" color="#f1e05a"> </el-progress>
+                    CSS
+                    <el-progress :percentage="13.7"></el-progress>
+                    HTML
+                    <el-progress :percentage="5.9" color="#f56c6c"> </el-progress>
+                </el-card>
+            </el-col>
+            <el-col :span="16">
+                <el-row :gutter="20" class="mgb20">
+                    <el-col :span="8">
+                        <el-card shadow="hover" :body-style="{ padding: '0px' }">
+                            <div class="grid-content grid-con-1">
+                                <i class="el-icon-s-custom grid-con-icon"></i>
+                                <div class="grid-cont-right">
+                                    <div class="grid-num">1234</div>
+                                    <div>用户访问量</div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-card shadow="hover" :body-style="{ padding: '0px' }">
+                            <div class="grid-content grid-con-2">
+                                <i class="el-icon-message-solid grid-con-icon"></i>
+                                <div class="grid-cont-right">
+                                    <div class="grid-num">321</div>
+                                    <div>系统消息</div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>
+                    <el-col :span="8">
+                        <el-card shadow="hover" :body-style="{ padding: '0px' }">
+                            <div class="grid-content grid-con-3">
+                                <i class="el-icon-goods grid-con-icon"></i>
+                                <div class="grid-cont-right">
+                                    <div class="grid-num">5000</div>
+                                    <div>数量</div>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>
+                </el-row>
+                <el-card shadow="hover" style="height:403px;">
+                    <div slot="header" class="clearfix">
+                        <span>待办事项</span>
+                        <el-button style="float: right; padding: 3px 0" type="text">添加</el-button>
+                    </div>
+                    <el-table :show-header="false" :data="todoList" style="width:100%;">
+                        <el-table-column width="40">
+                            <template slot-scope="scope">
+                                <el-checkbox v-model="scope.row.status"></el-checkbox>
+                            </template>
+                        </el-table-column>
+                        <el-table-column>
+                            <template slot-scope="scope">
+                                <div
+                                    class="todo-item"
+                                    :class="{
+                                        'todo-item-del': scope.row.status,
+                                    }"
+                                >
+                                    {{ scope.row.title }}
+                                </div>
+                            </template>
+                        </el-table-column>
+                        <el-table-column width="60">
+                            <template>
+                                <i class="el-icon-edit"></i>
+                                <i class="el-icon-delete"></i>
+                            </template>
+                        </el-table-column>
+                    </el-table>
+                </el-card>
+            </el-col>
+        </el-row>
+        <el-row :gutter="20">
+            <el-col :span="12">
+                <el-card shadow="hover">
+                    <schart ref="bar" class="schart" canvasId="bar" :options="options"></schart>
+                </el-card>
+            </el-col>
+            <el-col :span="12">
+                <el-card shadow="hover">
+                    <schart ref="line" class="schart" canvasId="line" :options="options2"></schart>
+                </el-card>
+            </el-col>
+        </el-row>
+    </div>
 </template>
 
 <script>
-export default {}
+import Schart from 'vue-schart'
+
+export default {
+    name: 'dashboard',
+    data() {
+        return {
+            name: localStorage.getItem('ms_username'),
+            todoList: [
+                {
+                    title: '今天要修复100个bug',
+                    status: false,
+                },
+                {
+                    title: '今天要修复100个bug',
+                    status: false,
+                },
+                {
+                    title: '今天要写100行代码加几个bug吧',
+                    status: false,
+                },
+                {
+                    title: '今天要修复100个bug',
+                    status: false,
+                },
+                {
+                    title: '今天要修复100个bug',
+                    status: true,
+                },
+                {
+                    title: '今天要写100行代码加几个bug吧',
+                    status: true,
+                },
+            ],
+            data: [
+                {
+                    name: '2020/09/04',
+                    value: 1003,
+                },
+                {
+                    name: '2020/09/05',
+                    value: 941,
+                },
+                {
+                    name: '2020/09/06',
+                    value: 1100,
+                },
+                {
+                    name: '2020/09/07',
+                    value: 921,
+                },
+                {
+                    name: '2020/09/08',
+                    value: 320,
+                },
+                {
+                    name: '2018/09/09',
+                    value: 325,
+                },
+                {
+                    name: '2020/09/10',
+                    value: 1087,
+                },
+            ],
+            options: {
+                type: 'bar',
+                title: {
+                    text: '最近一周各品类销售图',
+                },
+                xRorate: 25,
+                labels: ['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
+                datasets: [
+                    {
+                        label: '家电',
+                        data: [214, 218, 220, 140, 223, 234, 123],
+                    },
+                    {
+                        label: '百货',
+                        data: [124, 178, 190, 115, 180, 201, 188],
+                    },
+                    {
+                        label: '食品',
+                        data: [144, 198, 150, 235, 120, 129, 193],
+                    },
+                ],
+            },
+            options2: {
+                type: 'line',
+                title: {
+                    text: '最近几个月各品类销售趋势图',
+                },
+                labels: ['7月', '8月', '9月', '10月', '11月', '12月'],
+                datasets: [
+                    {
+                        label: '家电',
+                        data: [234, 278, 270, 190, 230, 274],
+                    },
+                    {
+                        label: '百货',
+                        data: [164, 178, 150, 135, 160, 142],
+                    },
+                    {
+                        label: '食品',
+                        data: [74, 118, 200, 235, 90, 103],
+                    },
+                ],
+            },
+        }
+    },
+    components: {
+        Schart,
+    },
+    computed: {
+        role() {
+            return this.name === 'admin' ? '超级管理员' : '普通用户'
+        },
+    },
+}
 </script>
 
-<style></style>
+<style scoped>
+.el-row {
+    margin-bottom: 20px;
+}
+.mgb20 {
+    margin-bottom: 20px;
+}
+.user-info {
+    display: flex;
+    align-items: center;
+    padding-bottom: 20px;
+    border-bottom: 2px solid #ccc;
+    margin-bottom: 20px;
+}
+.user-avator {
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+}
+.user-info-cont {
+    padding-left: 50px;
+    flex: 1;
+    font-size: 14px;
+    color: #999;
+}
+.user-info-cont div:first-child {
+    font-size: 30px;
+    color: #222;
+}
+.user-info-list {
+    font-size: 14px;
+    color: #999;
+    line-height: 25px;
+}
+.user-info-list span {
+    margin-left: 70px;
+}
+.grid-content {
+    display: flex;
+    align-items: center;
+    height: 100px;
+}
+.grid-con-1 .grid-con-icon {
+    background: rgb(45, 140, 240);
+}
+.grid-con-1 .grid-num {
+    color: rgb(45, 140, 240);
+}
+
+.grid-con-icon {
+    font-size: 50px;
+    width: 100px;
+    height: 100px;
+    text-align: center;
+    line-height: 100px;
+    color: #fff;
+}
+
+.grid-cont-right {
+    flex: 1;
+    text-align: center;
+    font-size: 14px;
+    color: #999;
+}
+.grid-num {
+    font-size: 30px;
+    font-weight: bold;
+}
+.grid-con-2 .grid-con-icon {
+    background: rgb(100, 213, 114);
+}
+.grid-con-2 .grid-num {
+    color: rgb(45, 140, 240);
+}
+.grid-con-3 .grid-con-icon {
+    background: rgb(242, 94, 67);
+}
+
+.grid-con-3 .grid-num {
+    color: rgb(242, 94, 67);
+}
+
+.todo-item {
+    font-size: 14px;
+}
+
+.todo-item-del {
+    text-decoration: line-through;
+    color: #999;
+}
+.schart {
+    width: 100%;
+    height: 300px;
+}
+</style>
